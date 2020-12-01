f9cd519dedbd4307043822f8e3db187519fb30fe
yerras <yerras@hotmail.com>
2020-12-01 00:53:09 +0000

    added a method in the main

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
 public class Application {
 
     public static void main(String[] args) {
+
         System.out.println("using maven");
+    }
+
+     public void dummyMethod(){
+         System.out.println("Inside the dummy method");
     }
 }
